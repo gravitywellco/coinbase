5a5ed7c343740f16f5eb771e11285c8ce64f104b
Cassidy Spring <79487947+beeauvin@users.noreply.github.com>
2024-07-10 06:04:34 -0700

    feat(deps): swap @std/dotenv for npm:dotenv
    Unfortunately the standard library dotenv implementation is not
    compatible with bun. This should fix that; though I would like to keep
    watch on this and change back if ever possible.

--- utilities/environment.utility.ts
+++ utilities/environment.utility.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import '@std/dotenv/load'
+import 'dotenv/config'
 
 /**
  * Unfortunatly, Deno and Bun have different ways of handling environment variables
