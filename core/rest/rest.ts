617757aa33a43af8bb42d538716572d8eefb92e7
Bee <79487947+beeauvin@users.noreply.github.com>
2024-07-15 04:22:22 -0700

    fix(core): pass the correct url (only path) to auth (#43)
    During the refactor for auth/requests the url being passed into auth
    methods went from just the path to the full URL of the request. This
    fixes that. x.x

--- core/rest/rest.ts
+++ core/rest/rest.ts
@@ -12,39 +12,45 @@ export * from './request/request.types.ts'
 
 /** Subclass for resource apis to be cleaner. Cleaner access to CoinbaseRequest */
 export class CoinbaseRest {
-  public readonly url: string
+  public readonly root: string
+  public readonly base: string
   public readonly auth: CoinbaseAuthConfig | undefined
 
   /**
    * @param base_url Base url for the rest instance, will be prepended to all requests.
    * @param auth Optional auth configuration for the rest instance, will be passed to all requests.
    */
-  constructor(base_url: string, auth?: CoinbaseAuthConfig) {
-    this.url = base_url
+  constructor(root: string, base: string, auth?: CoinbaseAuthConfig) {
+    this.root = root
+    this.base = base
     this.auth = auth
+  }
+
+  public url_data(path: string): RequestData {
+    return { url: this.root, path: this.base + path }
   }
 
   /** Make a GET request to the Coinbase API. */
   public async get<T>(path = '', query?: Query): Promise<T> {
-    const data: RequestData = { path: this.url + path, query }
+    const data: RequestData = { url: this.root, path: this.base + path, query }
     return await CoinbaseRequest.Get<T>(data, this.auth)
   }
 
   /** Make a POST request to the Coinbase API. */
   public async post<T>(path = '', body?: Record<string, unknown>): Promise<T> {
-    const data: RequestData = { path: this.url + path, body }
+    const data: RequestData = { ...this.url_data(path), body }
     return await CoinbaseRequest.Post<T>(data, this.auth)
   }
 
   /** Make a PUT request to the Coinbase API. */
   public async put<T>(path = '', body?: Record<string, unknown>): Promise<T> {
-    const data: RequestData = { path: this.url + path, body }
+    const data: RequestData = { ...this.url_data(path), body }
     return await CoinbaseRequest.Put<T>(data, this.auth)
   }
 
   /** Make a DELETE request to the Coinbase API. */
   public async delete<T>(path = '', query?: Query): Promise<T> {
-    const data: RequestData = { path: this.url + path, query }
+    const data: RequestData = { ...this.url_data(path), query }
     return await CoinbaseRequest.Delete<T>(data, this.auth)
   }
 }
--- resources/resource.ts
+++ resources/resource.ts
@@ -12,12 +12,11 @@ export abstract class CoinbaseResource {
 
   /**
    * @param config Pass along the configuration object from the parent instance.
-   * @param path Subpath to an api endpoint that all requests from this instance will make.
+   * @param base_path Subpath to an api endpoint that all requests from this instance will make.
    * @param has_auth Whether or not this request instance requires authentication.
    */
-  constructor(config: CoinbaseConfig, path: string, has_auth: boolean = false) {
-    const base_url = config.urls.api + path
+  constructor(config: CoinbaseConfig, base_path: string, has_auth: boolean = false) {
     const auth = has_auth ? config.auth : undefined
-    this.request = new CoinbaseRest(base_url, auth)
+    this.request = new CoinbaseRest(config.urls.api, base_path, auth)
   }
 }
